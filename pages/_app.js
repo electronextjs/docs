d93ba16c8c8fd2941ec3288a9d564e0f2e0c9802
André Malveira <www.carlosandre@gmail.com>
2022-03-27 15:28:02 -0300

    updated seo _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,25 @@ export default function Nextra({ Component, pageProps }) {
   return (
   <Container>
     <Head>
-    <link rel="icon" href="/electronext.logo.svg" />
+      <meta name="author"                  content="André Malveira<andremalveira.dev@gmail.com>"/>
+      <meta name="description"             content="Electron App Example with Next.js + Typescript"/>
+      <meta name="keywords"                content="electron-vibrancy electronext electronjs nextjs typescript"/>
+
+      <meta name="twitter:title"           content="ElectroNext.js"/>
+      <meta name="twitter:description "    content="Electron App Example with Next.js + Typescript"/>
+      <meta name="twitter:image:src"       content="https://electronextjs.github.io/.github/public/electronext.logo.png"/>
+      <meta name="twitter:card"            content="summary"/>
+
+      <meta property="og:type"             content="website" />
+      <meta property="og:site_name"        content="electronextjs.vercel.app" /> 
+      <meta property="og:type"             content="website" />
+      <meta property="og:url"              content="https://electronextjs.vercel.app" />
+      <meta property="og:title"            content="ElectroNext.js"/>
+      <meta property="og:description"      content="Electron App Example with Next.js + Typescript"/>
+      <meta property="og:image"            content="https://electronextjs.github.io/.github/public/electronext.logo.png"/>
+
+      <link rel="icon" href="/electronext.logo.svg" />
+
     </Head>
     <GlobalStyle/>
     <Component {...pageProps} />
